a9502dc6dcc81899fdcef60a0accfca25cf8b7f1
Chestermo <b02310043@ntu.edu.tw>
2018-09-11 23:47:25 +0800

    'modify_import_route_and_add_model'

--- welfare/clients/views.py
+++ welfare/clients/views.py
@@ -8,13 +8,43 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 # Client imports
 from clients.utils.auth import login_required
-
+from clients.models import *
 
 def signin_page(request):
-    return render_to_response("sign/sign_in.html", locals())
+    if request.method == "POST":
+        email = request.POST.get('account_mail',None)
+        password = request.POST.get('password',None)
+        sign_data = Signup.query({"account_mail":email})
+        print(sign_data)
+        if password == sign_data[0]["password"]:
+            return HttpResponse("hello world index!!")
+    return render(request, "sign/sign_in.html", locals())
 
 def signup_page(request):
-    return render_to_response("sign/sign_up.html", locals())
+    if request.method == "POST":
+        email = request.POST.get('account_mail',None)
+        username = request.POST.get('account_username',None)
+        password = request.POST.get('password',None)
+        checked, content = check_signup_content(request)
+        if checked == True:
+            sign_context = Signup()
+            if not Signup.query({"account_mail":email}):
+                sign_context.insert(content)
+                return HttpResponse("succeed!") #need a template for succeed and 3 seconds later redirect
+            else:
+                return HttpResponse("email is registered!")
+        return HttpResponse("there is comething wrong in ur data!")
+    return render(request,"sign/sign_up.html", locals())
+
+def check_signup_content(request):
+    checked = True
+    content = {}
+    for key, value in request.POST.items():
+        if value is None and value.strip() == "":
+            checked = False
+        elif key != "csrfmiddlewaretoken":
+            content[key] = value
+    return checked, content
 
 # TODO:
 # Add login_required at utils
